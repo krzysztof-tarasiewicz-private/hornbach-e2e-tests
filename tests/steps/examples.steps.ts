7221cced50ed80c0984105b3442c0f9803a81a20
Krzysztof Tarasiewicz <krzysztof.tarasiewicz@cronn.de>
2022-12-13 11:44:06 +0100

    Update examples.steps.ts

--- tests/steps/examples.steps.ts
+++ tests/steps/examples.steps.ts
@@ -3,18 +3,15 @@ import { page } from "./world";
 import {expect} from "@playwright/test"
 
 
-  Given('the user is on the demo website', async function () {
-    //await expect(page.locator('.status')).toHaveText('Submitted');
-    await expect(page.locator('[id=login-button]')).toHaveText('Login');
-    
-  });
+Given('I am on Hornbach site', async () => {
+  await expect(page.locator('//h1')).toHaveText('Online Antragserfassung ProfiCard');
+  // Write code here that turns the phrase above into concrete actions
+});
 
-  When('she enters her account information', async function () {
-    await page.getByPlaceholder('Username').fill('standard_user');
-    await page.getByPlaceholder('Password').fill('secret_sauce');
-    await page.locator('[id=login-button]').click();
-  });
+When('I click Main Card link', async () => {
+  await page.locator(`//*[contains(@href, 'application/main')]`).click();
+});
 
-  Then('she is logged in on the product page', async function () {
-    await expect (page.locator('[id=add-to-cart-sauce-labs-onesie]')).toBeVisible();
-  });
+Then('I see Main Card Profi Card form', async () => {
+  await expect(page.locator('//*[contains(text(),"Firmierung")]')).toBeVisible();
+});
